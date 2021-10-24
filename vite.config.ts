190baddca456cb0b533493755ec4737808c72e6e
Igor Wessel <igor_wessel@hotmail.com>
2021-10-24 11:18:19 -0300

    chore: added build to generate types

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import path from 'path'
 import { defineConfig } from 'vite'
 import tsConfigpaths from 'vite-tsconfig-paths'
 import react from '@vitejs/plugin-react'
+import dts from 'vite-plugin-dts'
 import { EsLinter, linterPlugin } from 'vite-plugin-linter'
 
 // https://vitejs.dev/config/
@@ -12,6 +13,13 @@ export default defineConfig((configEnv) => ({
     linterPlugin({
       include: ['./{lib,src}/**/*.{ts,tsx}'],
       linters: [new EsLinter({ configEnv })],
+    }),
+    dts({
+      include: ['lib/main.tsx'],
+      beforeWriteFile: (filePath, content) => ({
+        filePath: filePath.replace(/(\/|\\)lib/, '').replace('main', 'rds'),
+        content,
+      }),
     }),
   ],
   build: {
